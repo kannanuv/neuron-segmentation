3bc753f52710fb3fd48678c86383ca1fba31954b
James Gornet <jamesgornet@james-mbp.cshl.edu>
2017-07-11 15:44:37 -0400

    Fixed pixel classification

--- neuronclassifier.py
+++ neuronclassifier.py
@@ -1,11 +1,11 @@
-#!/usr/bin/env python
 from skimage import io
 import keras
 from keras.models import Sequential, load_model
 from keras.layers import (Dense, Dropout, Activation, Flatten,
                           Conv3D, MaxPooling3D)
+from keras import backend
 import argparse
-
+import numpy as np
 
 class NeuronModel:
     """
@@ -34,12 +34,11 @@ class NeuronModel:
     def fit(self, generator, steps_per_epoch=32):
         self._model.fit_generator(generator, steps_per_epoch,
                                   epochs=100,
-                                  validation_split=0.1,
                                   class_weight={0: 1, 1: 1.5},
-                                  verbose=1)
+                                  verbose=2)
 
-    def evaluate(self, generator, steps_per_epoch):
-        self._model.evaluate_generator(generator, steps_per_epoch)
+    def evaluate(self, generator, steps):
+        self._model.evaluate_generator(generator, steps)
 
     def load(self, filepath):
         self._model.load_model(filepath)
@@ -54,15 +53,20 @@ class NeuronModel:
             for inputs_filename, targets_filename in zip(inputs_filenames,
                                                          targets_filenames):
                 inputs = self.load_tif(inputs_filename)
+                inputs = inputs.reshape(1, inputs.shape[0],
+                                        inputs.shape[1], inputs.shape[2], 1)
                 targets = self.load_tif(targets_filename)
+                targets = targets.reshape(1, targets.shape[0],
+                                          targets.shape[1], targets.shape[2], 1)
                 bounding_box_size = self.input_shape[0:3]
                 for box in self.boxes(targets, bounding_box_size):
                     ([x1, y1, z1], [x2, y2, z2]) = box
-                    subtargets = targets[z1:z2, y1:y2, x1:x2]
-                    mean = subtargets.mean()
+                    mean = targets[:, z1:z2, y1:y2, x1:x2, :].mean()
                     if mean < 0.95 and mean > 0.05:
-                        subtargets = targets[z1:z2, y1:y2, x1:x2]
-                        subinputs = inputs[z1:z2, y1:y2, x1:x2]
+                        subtargets = np.array([targets[0, (z1+z2)/2,
+                                                       (y1+y2)/2,
+                                                       (x1+x2)/2, 0]])
+                        subinputs = inputs[:, z1:z2, y1:y2, x1:x2, :]
                         yield (subinputs, subtargets)
 
     def load_tif(self, filename, dtype=None):
@@ -76,13 +80,13 @@ class NeuronModel:
         (L, H, W) = bounding_box_size
         while True:
             x += W
-            if x + W > data.shape[2]:
+            if x + W > data.shape[3]:
                 x = 0
                 y += H
-            if y + H > data.shape[1]:
+            if y + H > data.shape[2]:
                 y = 0
                 z += L
-            if z + L > data.shape[0]:
+            if z + L > data.shape[1]:
                 z = 0
                 raise StopIteration
             yield ([x, y, z], [x+W, y+H, z+L])
@@ -101,7 +105,7 @@ def test():
     nn.fit(train_generator)
 
     test_generator = nn.generator(TEST_INPUTS, TEST_TARGETS)
-    nn.evaluate(test_generator)
+    nn.evaluate(test_generator, steps=10)
 
     nn.save('model.h5')
 
